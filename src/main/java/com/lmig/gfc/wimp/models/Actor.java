178da6e79fa985bd5512f9652acf5c11b60bcc71
Lisa Forrest <lisam.forrest@libertymutual.com>
2017-12-05 16:04:38 -0500

    Wimp project

---
+++ src/main/java/com/lmig/gfc/wimp/models/Actor.java
@@ -0,0 +1,73 @@
+package com.lmig.gfc.wimp.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Actor {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(length = 75, nullable = false)
+	private String firstName;
+
+	@Column(length = 75, nullable = true)
+	private String lastName;
+
+	@Column(nullable = true)
+	private Long activeSinceYear;
+
+	@Column(nullable = true)
+	private Date birthDate;
+
+	public Actor() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Long getActiveSinceYear() {
+		return activeSinceYear;
+	}
+
+	public void setActiveSinceYear(Long activeSinceYear) {
+		this.activeSinceYear = activeSinceYear;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+}
---
+++ src/main/java/com/lmig/gfc/wimp/models/Movie.java
@@ -0,0 +1,73 @@
+package com.lmig.gfc.wimp.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+	@Entity
+	public class Movie {
+		
+		@Id
+		@GeneratedValue(strategy = GenerationType.AUTO)
+		private Long id;
+		
+		@Column(length=300, nullable=false)
+		private String title;
+		
+		@Column(nullable=true)
+		private Long releaseDate; 
+		
+		@Column(nullable=true)		
+		private Long budget;
+		
+		@Column(length=500, nullable=false)
+		private String distributor;
+
+		public Movie() {
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getTitle() {
+			return title;
+		}
+
+		public void setTitle(String title) {
+			this.title = title;
+		}
+
+		public Long getReleaseDate() {
+			return releaseDate;
+		}
+
+		public void setReleaseDate(Long releaseDate) {
+			this.releaseDate = releaseDate;
+		}
+
+		public Long getBudget() {
+			return budget;
+		}
+
+		public void setBudget(Long budget) {
+			this.budget = budget;
+		}
+
+		public String getDistributor() {
+			return distributor;
+		}
+
+		public void setDistributor(String distributor) {
+			this.distributor = distributor;
+		}
+
+
+}
